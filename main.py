5d62b59fb30ed51f6c798e5c2b43c99e310156d9
guljamol2005 <namozovaguljamol0111@gamil.com>
2025-12-04 12:27:17 +0500

    misol uchun

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ def bin_to_dec(binary: str) -> int:
 
 def bin_to_hex(binary: str) -> str:
     return hex(int(binary, 2))[2:].upper()
+
+print(bin_to_oct("1011"))     # → 13
+print(bin_to_dec("1011"))     # → 11
+print(bin_to_hex("1011"))     # → B
+
+print(oct_to_bin("77"))       # → 111111
+print(dec_to_bin(25))         # → 11001
+print(hex_to_bin("FF"))       # → 11111111
+
